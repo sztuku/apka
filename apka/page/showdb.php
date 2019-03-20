19e9796a01f8acaf5292387665eb567b4bac7e3a
sztuku <48708946+sztuku@users.noreply.github.com>
2019-03-20 08:06:01 +0100

    Update showdb.php

--- apka/page/showdb.php
+++ apka/page/showdb.php
@@ -95,94 +95,67 @@ if($_SESSION['status']==TRUE && $_SESSION['userid']==1)
    $r = mysqli_fetch_row($result);
    $numrows = $r[0];
 
-   // number of rows to show per page
+  
    $rowsperpage = 10;
-   // find out total pages
+  
    $totalpages = ceil($numrows / $rowsperpage);
 
-   // get the current page or set a default
    if (isset($_GET['currentpage']) && is_numeric($_GET['currentpage'])) {
-      // cast var as int
       $currentpage = (int) $_GET['currentpage'];
    } else {
-      // default page num
       $currentpage = 1;
-   } // end if
+   } 
 
-   // if current page is greater than total pages...
    if ($currentpage > $totalpages) {
-      // set current page to last page
       $currentpage = $totalpages;
-   } // end if
-   // if current page is less than first page...
+   } 
    if ($currentpage < 1) {
-      // set current page to first page
       $currentpage = 1;
-   } // end if
+   } 
 
-   // the offset of the list, based on current page 
    $offset = ($currentpage - 1) * $rowsperpage;
    
 
-   // get the info from the db 
    $sql = 'SELECT `Id`,`data`,`User_id` FROM `idzamowienia`  
    ORDER BY '.$sortby.'  '.$way.'  
    LIMIT '.$offset.', '.$rowsperpage.' ';
 
    $result = mysqli_query($connection,$sql);
 
-   // while there are rows to be fetched...
    while ($list = mysqli_fetch_assoc($result)) {
-      // echo data
       echo '<tr> 
       <td> <a  class="rowhref" href="index.php?page=showelements&Id='.$list['Id'].'">'.$list['Id'].'<a/></td>
 
       <td> <a  class="rowhref" href="index.php?page=showelements&Id='.$list['Id'].'">'.$list['data'].'<a/></td> 
       <td> <a  class="rowhref" href="index.php?page=showelements&Id='.$list['Id'].'">'.$list['User_id'].'<a/></td> 
       </tr>';
-   } // end while
+   } 
 
-   /******  build the pagination links ******/
-   // range of num links to show
    $range = 3;
 
-   // if not on page 1, don't show back links
    echo "<div id='znaczki'>";
    if ($currentpage > 1) {
-      // show << link to go back to page 1
       echo " <a href='index.php?page=showdb&currentpage=1&way=".$sort."&sortby=".$sortby."'><<</a> ";
-      // get previous page num
       $prevpage = $currentpage - 1;
-      // show < link to go back to 1 page
       echo " <a href='index.php?page=showdb&currentpage=$prevpage&way=".$sort."&sortby=".$sortby."'><</a> ";
-   } // end if 
+   }  
 
-   // loop to show links to range of pages around current page
 
    for ($x = ($currentpage - $range); $x < (($currentpage + $range) + 1); $x++) {
-      // if it's a valid page number...
       if (($x > 0) && ($x <= $totalpages)) {
-         // if we're on current page...
          if ($x == $currentpage) {
-            // 'highlight' it but don't make a link
             echo " [<b>$x</b>] ";
-         // if not current page...
          } else {
-            // make it a link
             echo " <a href='index.php?page=showdb&currentpage=$x&way=".$sort."&sortby=".$sortby."'>$x</a> ";
-         } // end else
-      } // end if 
-   } // end for
+         }
+      }  
+   } 
 
-   // if not on last page, show forward and last page links        
    if ($currentpage != $totalpages) {
-      // get next page
       $nextpage = $currentpage + 1;
-      // echo forward link for next page 
       echo " <a href='index.php?page=showdb&currentpage=$nextpage&way=".$sort."&sortby=".$sortby."'>></a> ";
-      // echo forward link for lastpage
       echo " <a href='index.php?page=showdb&currentpage=$totalpages&way=".$sort."&sortby=".$sortby."'>>></a> ";
-   } // end if
+   } 
    echo "</div>";
  
 }else
@@ -200,93 +173,67 @@ if($_SESSION['status']==TRUE && $_SESSION['userid']==1)
       $r = mysqli_fetch_row($result);
       $numrows = $r[0];
 
-      // number of rows to show per page
+     
       $rowsperpage = 10;
-      // find out total pages
+     
       $totalpages = ceil($numrows / $rowsperpage);
 
-      // get the current page or set a default
-      if (isset($_GET['currentpage']) && is_numeric($_GET['currentpage'])) {
-         // cast var as int
-         $currentpage = (int) $_GET['currentpage'];
+         if (isset($_GET['currentpage']) && is_numeric($_GET['currentpage'])) {
+            $currentpage = (int) $_GET['currentpage'];
       } else {
-         // default page num
-         $currentpage = 1;
-      } // end if
+            $currentpage = 1;
+      } 
 
-      // if current page is greater than total pages...
-      if ($currentpage > $totalpages) {
-         // set current page to last page
-         $currentpage = $totalpages;
-      } // end if
-      // if current page is less than first page...
-      if ($currentpage < 1) {
-         // set current page to first page
-         $currentpage = 1;
-      } // end if
+         if ($currentpage > $totalpages) {
+            $currentpage = $totalpages;
+      } 
+         if ($currentpage < 1) {
+            $currentpage = 1;
+      } 
 
-      // the offset of the list, based on current page 
-      $offset = ($currentpage - 1) * $rowsperpage;
+         $offset = ($currentpage - 1) * $rowsperpage;
 
-      // get the info from the db 
-      $sql = 'SELECT `Id`,`data` FROM `idzamowienia`  
+         $sql = 'SELECT `Id`,`data` FROM `idzamowienia`  
       WHERE `User_id`="'.$_SESSION['userid'].'"
       ORDER BY '.$sortby.'  '.$way.'  
       LIMIT '.$offset.', '.$rowsperpage.' ';
 
       $result = mysqli_query($connection,$sql);
 
-      // while there are rows to be fetched...
-      while ($list = mysqli_fetch_assoc($result)) {
-         // echo data
-         echo '<tr> 
+         while ($list = mysqli_fetch_assoc($result)) {
+            echo '<tr> 
          <td> <a  class="rowhref" href="index.php?page=showelements&Id='.$list['Id'].'">'.$list['Id'].'<a/></td>
          
          <td> <a  class="rowhref" href="index.php?page=showelements&Id='.$list['Id'].'">'.$list['data'].'<a/></td> 
          </tr>';
-      } // end while
+      } 
 
-      /******  build the pagination links ******/
-      // range of num links to show
-      $range = 3;
+            $range = 3;
 
-      // if not on page 1, don't show back links
-      echo "<div id='znaczki'>";
+         echo "<div id='znaczki'>";
       if ($currentpage > 1) {
-         // show << link to go back to page 1 
+       
          echo " <a href='index.php?page=showdb&currentpage=1&way=".$sort."&sortby=".$sortby."'><<</a> ";
-         // get previous page num
-         $prevpage = $currentpage - 1;
-         // show < link to go back to 1 page
-         echo " <a href='index.php?page=showdb&currentpage=$prevpage&way=".$sort."&sortby=".$sortby."'><</a> ";
-      } // end if 
+            $prevpage = $currentpage - 1;
+            echo " <a href='index.php?page=showdb&currentpage=$prevpage&way=".$sort."&sortby=".$sortby."'><</a> ";
+      }  
 
-      // loop to show links to range of pages around current page
-
+   
       for ($x = ($currentpage - $range); $x < (($currentpage + $range) + 1); $x++) {
-         // if it's a valid page number...
-         if (($x > 0) && ($x <= $totalpages)) {
-            // if we're on current page...
-            if ($x == $currentpage) {
-               // 'highlight' it but don't make a link
-               echo " [<b>$x</b>] ";
-            // if not current page...
-            } else {
-               // make it a link
-               echo " <a href='index.php?page=showdb&currentpage=$x&way=".$sort."&sortby=".$sortby."'>$x</a> ";
-            } // end else
-         } // end if 
-      } // end for
+            if (($x > 0) && ($x <= $totalpages)) {
+               if ($x == $currentpage) {
+                  echo " [<b>$x</b>] ";
+               } else {
+                  echo " <a href='index.php?page=showdb&currentpage=$x&way=".$sort."&sortby=".$sortby."'>$x</a> ";
+            }
+         }  
+      } 
 
-      // if not on last page, show forward and last page links        
-      if ($currentpage != $totalpages) {
-         // get next page
-         $nextpage = $currentpage + 1;
-         // echo forward link for next page 
-         echo " <a href='index.php?page=showdb&currentpage=$nextpage&way=".$sort."&sortby=".$sortby."'>></a> ";
-         // echo forward link for lastpage
-         echo " <a href='index.php?page=showdb&currentpage=$totalpages&way=".$sort."&sortby=".$sortby."'>>></a> ";
-      } // end if
+         if ($currentpage != $totalpages) {
+            $nextpage = $currentpage + 1;
+            echo " <a href='index.php?page=showdb&currentpage=$nextpage&way=".$sort."&sortby=".$sortby."'>></a> ";
+            echo " <a href='index.php?page=showdb&currentpage=$totalpages&way=".$sort."&sortby=".$sortby."'>>></a> ";
+      } 
       echo "</div>";
 }
 ?>
@@ -294,6 +241,6 @@ if($_SESSION['status']==TRUE && $_SESSION['userid']==1)
     
 
 
-    <script src="lista.js"></script>
+   
   </body>
 </html>
